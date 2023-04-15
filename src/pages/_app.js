3209768566cbd54e19500d76cfb89336183cfc2f
gonza0086 <hgonzalo2000@gmail.com>
2023-04-15 00:03:26 -0300

    initial structure

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css'
+// Components
+import Head from 'next/head';
+
+// Styles
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>Bonnie & Clyde</title>
+                <meta name='description' content='Bonnie & Clyde app - Gonzalo Hernandez & Tomas Mudano' />
+                <meta name='viewport' content='width=device-width, initial-scale=1' />
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
