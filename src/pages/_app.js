a1e802ca515eb7a62b1c7d36f017d16e45182bba
gonza0086 <hgonzalo2000@gmail.com>
2023-04-15 21:00:44 -0300

    mui theme provider, nextjs fonts and global css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,29 @@
 // Components
 import Head from 'next/head';
 
+// MUI
+import { ThemeProvider, createTheme } from '@mui/material';
+import { deepPurple, pink } from '@mui/material/colors';
+
+// Font
+import localFont from 'next/font/local';
+const alkatra = localFont({ src: '../assets/Alkatra-VariableFont_wght.ttf' });
+
 // Styles
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: deepPurple[400],
+            },
+            secondary: {
+                main: pink[400],
+            },
+        },
+    });
+
     return (
         <>
             <Head>
@@ -13,7 +32,9 @@ export default function App({ Component, pageProps }) {
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
                 <link rel='icon' href='/favicon.ico' />
             </Head>
-            <Component {...pageProps} />
+            <ThemeProvider theme={theme}>
+                <Component {...pageProps} />
+            </ThemeProvider>
         </>
     );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,18 @@
+import { AppBar, Button, Toolbar } from '@mui/material';
+
 export default function Home() {
-    return <div>Bonnie & Clyde</div>;
+    return (
+        <div>
+            <AppBar position='static'>
+                <Toolbar style={{ justifyContent: 'space-between' }}>
+                    <h3>Title example</h3>
+                    <p>Text example</p>
+                    <Button variant='contained' color='secondary'>
+                        Login
+                    </Button>
+                </Toolbar>
+            </AppBar>
+            Bonnie & Clyde
+        </div>
+    );
 }
