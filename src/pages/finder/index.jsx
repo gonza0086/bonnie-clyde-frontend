453efb2ecc89a04815cb7bc46cfac40712348971
gonza0086 <hgonzalo2000@gmail.com>
2023-04-27 15:11:52 -0300

    search style

--- src/pages/finder/index.jsx
+++ src/pages/finder/index.jsx
@@ -7,9 +7,17 @@ export default function Finder() {
         <>
             <div className='floating-container'>
                 <Title>Finder</Title>
-                <div className='search-input-container'>
-                    <InputBase />
-                    <IconButton type='button'>
+                <div className='search-input-container' style={{ display: 'inline-flex' }}>
+                    <InputBase
+                        style={{
+                            paddingInline: '0.5rem',
+                            border: '0.5px solid var(--grey-500)',
+                            borderRight: 'none',
+                            borderRadius: '5px 0 0 5px',
+                        }}
+                        placeholder='test'
+                    />
+                    <IconButton type='button' style={{ borderRadius: '0 5px 5px 0', backgroundColor: 'var(--secondary-color)' }}>
                         <SearchIcon />
                     </IconButton>
                 </div>
